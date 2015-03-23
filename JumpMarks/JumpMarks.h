fd1cd073d6d153847350053dd53fe6c6cf21b00e
Merrick Poon <merrickp@zoosk.com>
2015-03-22 18:30:30 -0700

    Initial commit

---
+++ JumpMarks/JumpMarks.h
@@ -0,0 +1,16 @@
+//
+//  JumpMarks.h
+//  JumpMarks
+//
+//  Created by Merrick Poon on 3/22/15.
+//  Copyright (c) 2015 Merrick Poon. All rights reserved.
+//
+
+#import <AppKit/AppKit.h>
+
+@interface JumpMarks : NSObject
+
++ (instancetype)sharedPlugin;
+
+@property (nonatomic, strong, readonly) NSBundle* bundle;
+@end
