9182bc6892954e7a347cc13d3870eaa0d4ad6bda
Merrick Poon <merrickpoon@gmail.com>
2016-10-03 18:04:46 -0700

    - Update compatibility UUID for Xcode 8 migration. Do not merge into master into gutter dot is restored for mark.

--- JumpMarksTest/JumpMarksTest.m
+++ JumpMarksTest/JumpMarksTest.m
@@ -8,14 +8,14 @@
 
 #import <Cocoa/Cocoa.h>
 #import "JumpMark.h"
-#import <Specta/Specta.h>
-#import <Expecta/Expecta.h>
-#import <OCMock/OCMock.h>
+//#import <Specta/Specta.h>
+//#import <Expecta/Expecta.h>
+//#import <OCMock/OCMock.h>
 
 static NSString *const JUMPMARKS_PATH = @"Library/Application Support/Developer/Shared/Xcode/Plug-ins/JumpMarks.xcplugin";
 
-SpecBegin(JumpMarksTests)
+//SpecBegin(JumpMarksTests)
 
 //NSBundle *pluginBundle = [NSBundle bundleWithPath:[NSHomeDirectory() stringByAppendingPathComponent:JUMPMARKS_PATH]];
 
-SpecEnd
+//SpecEnd
